47f47feedc2e7a5cb6a8a25d4890c69358aed3d2
sahalshaikhh <shaikhsahal308@gmail.com>
2023-08-01 10:03:36 +0530

    Authentication using PassportsJs, session, passportLocal and PassportLocalMongoose

--- app.js
+++ app.js
@@ -5,14 +5,39 @@ const bodyParser = require('body-parser');
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const Encryption = require("mongoose-field-encryption").fieldEncryption;
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+
+// Level 4 Authentication: bycrypt
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+
+// Level 5 
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose")
 
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// Session
+
+app.use(session({
+    secret: "MySecretisSecret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60 * 30 * 1000, // Keeping the sessions,
+        httpOnly: true   // Prevent access to cookies from client side script
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // Databse Checking
 
@@ -26,6 +51,8 @@ const NewUserSchema = new mongoose.Schema({
     password: String
 });
 
+NewUserSchema.plugin(passportLocalMongoose);
+
 
 // NewUserSchema.plugin(Encryption, { fields: ["password"], secret: process.env.SECRET });(LEVEL:2 Security)
 
@@ -36,6 +63,11 @@ const NewUserSchema = new mongoose.Schema({
 // Making model
 
 const NewUser = mongoose.model("User", NewUserSchema);
+
+passport.use(NewUser.createStrategy());
+
+passport.serializeUser(NewUser.serializeUser());
+passport.deserializeUser(NewUser.deserializeUser());
 
 app.get("/", (req, res) => {
     res.render('home')
@@ -49,48 +81,57 @@ app.get("/login", (req, res) => {
     res.render("login")
 });
 
+app.get("/secrets", (req, res) => {
+    // Check for authentication before allowing access to secrets page
+    if (req.isAuthenticated()) {
+        res.render("secrets")
+    } else {
+        res.redirect("/login")
+    }
+})
+
+app.get("/logout", (req, res) => {
+    req.logout(() => {
+        res.redirect("/")
+    })
+})
+
 app.post("/register", (req, res) => {
 
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-        // Store hash in your password DB.
-        const user = req.body.username;
-        const Password = hash;
-        const newUser = new NewUser({
-            "userName": user,
-            "password": Password
-        })
-        newUser.save()
-            .then(() => {
-                res.render("secrets")
+    NewUser.register({ username: req.body.username }, req.body.password, (err, user) => {
+        if (err) {
+            console.log(err);
+            res.redirect("/register")
+        } else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secrets");
             })
-    });
-
-
-
+        }
+    })
 });
 
 app.post("/login", (req, res) => {
-    const user = req.body.username;
-    const password = req.body.password;
-
-    NewUser.findOne({
-        userName: user,
-    })
-        .then((foundData) => {
-            if (foundData) {
-                bcrypt.compare(password, foundData.password, function (err, result) {
-                    // result == true
-                    if (result === true) {
-                        res.render("secrets")
-                    } else {
-                        res.send("Password incorret")
-                    }
-                });
+    // Use passport.authenticate middleware directly here
+    passport.authenticate("local", (err, user, info) => {
+        if (err) {
+            console.log("ERROR" + err);
+            return;
+        }
+        if (!user) {
+            // Authentication failed, handle the error or redirect to login again.
+            console.log("Authentication failed!");
+            return res.redirect("/login");
+        }
+        req.login(user, (err) => {
+            if (err) {
+                console.log("ERROR" + err);
             } else {
-                res.send("Please Register first")
+                // Successful authentication, redirect to the secrets page.
+                res.redirect("/secrets");
             }
-        })
-})
+        });
+    })(req, res);
+});
 
 
 app.listen("3000", (req, res) => {
